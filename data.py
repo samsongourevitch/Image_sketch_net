6d6099a91bde6222b3c34fc9bad55d1d51eb901e
samsongourevitch <samson.gourevitch@gmail.com>
2024-11-07 15:40:08 +0100

    "Data transform"

--- data.py
+++ data.py
@@ -14,12 +14,12 @@ data_transforms = transforms.Compose(
 data_transforms_resnet = transforms.Compose(
     [
     transforms.Resize((224, 224)),               
-    # transforms.RandomHorizontalFlip(p=0.5),       
-    # transforms.RandomRotation(degrees=10),      
-    # transforms.ColorJitter(brightness=0.2,      
-    #                        contrast=0.2,
-    #                        saturation=0.2,
-    #                        hue=0.1),
+    transforms.RandomHorizontalFlip(p=0.5),       
+    transforms.RandomRotation(degrees=10),      
+    transforms.ColorJitter(brightness=0.2,      
+                           contrast=0.2,
+                           saturation=0.2,
+                           hue=0.1),
     transforms.ToTensor(),                        
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]
