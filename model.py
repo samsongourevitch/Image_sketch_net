e0e7c0bd039fe8d8b3b933532a1c5ca9840790bb
samsongourevitch <samson.gourevitch@gmail.com>
2024-11-22 13:16:55 +0100

    debug

--- model.py
+++ model.py
@@ -138,7 +138,7 @@ class MetaModel(nn.Module):
                 model.eval()
                 model.to(device)
                 outputs.append(model(x))
-                print(outputs.shape)
+                print(model(x).shape)
             stacked_outputs = torch.stack(outputs, dim=2)
         meta_input = stacked_outputs.view(stacked_outputs.size(0), -1)
         return self.fc(meta_input)
