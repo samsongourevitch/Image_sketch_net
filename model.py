a27ebe76a465b3132fe4a9035e0fb006e6d77ad3
samsongourevitch <samson.gourevitch@gmail.com>
2024-11-22 13:12:37 +0100

    input dim changed

--- model.py
+++ model.py
@@ -138,5 +138,6 @@ class MetaModel(nn.Module):
                 model.eval()
                 model.to(device)
                 outputs.append(model(x))
-            outputs = torch.stack(outputs, dim=1)
-        return self.fc(outputs)
+            stacked_outputs = torch.stack(outputs, dim=2)
+        meta_input = stacked_outputs.view(stacked_outputs.size(0), -1)
+        return self.fc(meta_input)
